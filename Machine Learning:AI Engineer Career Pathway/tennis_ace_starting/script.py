0db7cf0dbddba5b7082e4f84a6edfca6c3340954
anishsharma21 <anisharma2103@gmail.com>
2024-06-05 18:36:54 +1000

    - Phase of tennis_ace_starting challenge which involves single and multiple linear regression techniques

--- Machine Learning:AI Engineer Career Pathway/tennis_ace_starting/script.py
+++ Machine Learning:AI Engineer Career Pathway/tennis_ace_starting/script.py
@@ -1,85 +1,16 @@
-import codecademylib3_seaborn
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 # load and investigate the data here:
-
-
-
-
-
-
-
-
-# perform exploratory analysis here:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tennis = pd.read_csv('./tennis_stats.csv')
+x = tennis['ServiceGamesPlayed']
+y = tennis['ReturnGamesPlayed']
 
 ## perform single feature linear regressions here:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8, test_size=0.2)
 
 ## perform two feature linear regressions here:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ## perform multiple feature linear regressions here:
