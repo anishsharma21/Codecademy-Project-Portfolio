1924e95d4dd286c3006e5010b1377860fad9b471
anishsharma21 <anisharma2103@gmail.com>
2024-06-18 17:03:14 +1000

    - implemented quick sort with just pointers (constant space time complexity)

--- Data Structures and Algorithms with Python/QuickSort.py
+++ Data Structures and Algorithms with Python/QuickSort.py
@@ -12,6 +12,23 @@ def quick_sort(arr):
         greater_than = quick_sort(greater_than)
     return less_than + [pivot_val] + greater_than
 
+def quick_sort_with_pointers(arr, start, end):
+    if start >= end:
+        return
+    pivot_idx = (start + end) // 2
+    pivot_element = arr[pivot_idx]
+    arr[end], arr[pivot_idx] = arr[pivot_idx], arr[end]
+    less_than_pointer = start
+    for i in range(start, end):
+        if arr[i] < pivot_element:
+            arr[i], arr[less_than_pointer] = arr[less_than_pointer], arr[i]
+            less_than_pointer += 1
+    arr[end], arr[less_than_pointer] = arr[less_than_pointer], arr[end]
+    quick_sort_with_pointers(arr, start, less_than_pointer - 1)
+    quick_sort_with_pointers(arr, less_than_pointer + 1, end)
+
 my_list = [4, 3, 7, 2, 1, 5, 22, 0, 0, 22, 2]
-print(quick_sort(my_list))
+# print(quick_sort(my_list))
+quick_sort_with_pointers(my_list, 0, len(my_list)-1)
+print(my_list)
 # nice
